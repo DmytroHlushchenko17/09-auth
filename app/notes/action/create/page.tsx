32d66af2089e757d617464be6247ca7006302e97
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2026-01-16 20:52:48 +0200

    Initial commit

---
+++ app/notes/action/create/page.tsx
@@ -0,0 +1,32 @@
+import NoteForm from "@/components/NoteForm/NoteForm";
+import css from "./CreateNote.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create notes",
+  description: "Create notes",
+  openGraph: {
+    title: "Create notes",
+    description: "Create notes",
+    url: "https://08-zustand-livid-sigma.vercel.app/notes/action/create",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Note Hub",
+      },
+    ],
+  },
+};
+
+export default function CreateNote() {
+  return (
+    <main className={css.main}>
+      <div className={css.container}>
+        <h1 className={css.title}>Create note</h1>
+        <NoteForm />
+      </div>
+    </main>
+  );
+}
